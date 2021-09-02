e4925f598a20e1da3d325755feea3f3b49d7eb1a
High_Lander <codywmarkham@gmail.com>
2021-09-02 17:20:43 -0500

    bugfix

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const {OAuth2Client} = require('google-auth-library');
-const CLIENT_ID = '770425769909-1b53dbhequvdv35mnu4o28mjn7mo7jnr.apps.googleusercontent.com'
-const client = new OAuth2Client(CLIENT_ID)
+
 
 router.get('/', (req, res) => {
     res.render('homepage');
--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,7 @@ const loginRoutes = require('./loginRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const apiRoutes = require('./api');
 
-const {OAuth2Client} = require('google-auth-library');
-const CLIENT_ID = '770425769909-1b53dbhequvdv35mnu4o28mjn7mo7jnr.apps.googleusercontent.com'
-const client = new OAuth2Client(CLIENT_ID)
+
 
 router.use('/', homeRoutes);
 router.use('/login', loginRoutes);
