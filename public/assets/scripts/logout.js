cbc4a3895a669eced4498f0de73b5652500666d2
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-09-02 00:13:36 -0500

    Added logout capability to the user experience

---
+++ public/assets/scripts/logout.js
@@ -0,0 +1,15 @@
+// Execute signup for current user
+$('#logout-submit').on('click', async function (event) {
+    console.log('working');
+
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/login');
+    } else {
+        alert(response.statusText);
+    }
+});
