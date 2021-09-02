51d2750805fad7d3f31f5a0bb411fe86b3e6b7ba
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-09-02 15:23:05 -0500

    Added to the dashboard the todos by plan

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
-const { Events, Plans } = require('../models');
+const { Events, Plans, Todos } = require('../models');
 
 router.get('/', withAuth, async (req, res) => {
     try {
@@ -45,8 +45,6 @@ router.get('/events/:id', withAuth, async (req, res) => {
                 events_id: req.params.id
             },          
         });
-
-        console.log(eventData);
 
         if (eventData) {
             res.render('dashboard', {
