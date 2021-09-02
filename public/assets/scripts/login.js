ca7446c57373b10e9bb3119dd58de81185a2d2ee
High_Lander <codywmarkham@gmail.com>
2021-09-02 15:49:33 -0500

    header remove

--- public/assets/scripts/login.js
+++ public/assets/scripts/login.js
@@ -73,7 +73,6 @@ function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/login');
-    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = function() {
         console.log('Signed in as: ' + xhr.responseText);
         if(xhr.responseText == 'success'){
